09932b54a7c07d81dff93357339949ae004d8c1e
Steve McDougall <juststevemcd@gmail.com>
2023-03-17 19:57:57 +0000

    File Sizes

--- src/ValueObjects/FileSize.php
+++ src/ValueObjects/FileSize.php
@@ -13,6 +13,26 @@ final class FileSize implements ValueObjectContract
         public readonly int $size,
     ) {}
 
+    public function bytes(): int
+    {
+        return $this->size;
+    }
+
+    public function kilobytes(): int
+    {
+        return $this->size * 1024;
+    }
+
+    public function megabytes(): int
+    {
+        return $this->kilobytes() * 1024;
+    }
+
+    public function gigabytes(): int
+    {
+        return $this->megabytes() * 1024;
+    }
+
     public static function fromPath(string $path): FileSize
     {
         if (! file_exists(filename: $path)) {
--- tests/ValueObjects/FileSizeTest.php
+++ tests/ValueObjects/FileSizeTest.php
@@ -30,3 +30,38 @@ it('throws an exception if the file does not exist', function (string $path): vo
         path: $path,
     );
 })->with('files')->throws(FileDoesNotExist::class);
+
+it('can get the size in bytes', function (): void {
+    $file = __DIR__ . '/../../composer.json';
+
+    expect(
+        FileSize::fromPath(
+            path: $file,
+        )->bytes(),
+    )->toBeInt()->toEqual(\filesize($file));
+});
+
+it('can get the size in kilobytes', function (): void {
+    expect(
+        (new FileSize(
+            size: 1,
+        ))->kilobytes(),
+    )->toBeInt()->toEqual(1024);
+});
+
+it('can get the size in megabytes', function (): void {
+    expect(
+        (new FileSize(
+            size: 1,
+        ))->megabytes(),
+    )->toBeInt()->toEqual(1024 * 1024);
+});
+
+it('can get the size in gigabytes', function (): void {
+    expect(
+        (new FileSize(
+            size: 1,
+        ))->gigabytes(),
+    )->toBeInt()->toEqual(1024 * 1024 * 1024);
+});
+
